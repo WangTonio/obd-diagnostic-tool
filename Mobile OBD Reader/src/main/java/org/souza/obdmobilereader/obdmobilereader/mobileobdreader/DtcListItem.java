a50781037cbacbb68300a7786ce6046119d672e5
ajsouza88@gmail.com <ajsouza88@gmail.com@9e2c58d2-b951-7405-26da-efba02381a61>
2014-04-03 04:36:48 +0000

    save point 	-main screen 	-gauges(not yet done) 	-dtc lookup(not yet done

---
+++ Mobile OBD Reader/src/main/java/org/souza/obdmobilereader/obdmobilereader/mobileobdreader/DtcListItem.java
@@ -0,0 +1,25 @@
+package org.souza.obdmobilereader.obdmobilereader.mobileobdreader;
+
+
+public class DtcListItem {
+    String code;
+    String summary;
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+}
