9d1a44368b91dbe4c1ec2e4427517196d97b30c9
ajsouza88@gmail.com <ajsouza88@gmail.com@9e2c58d2-b951-7405-26da-efba02381a61>
2014-05-22 00:50:24 +0000

    save point (5/21/2014) Added new Custom Gauges Reworked piper for sending data to gauges Finished DTC Lookup Page

---
+++ branches/Souza52114/src/main/java/org/souza/obdmobilereader/obdmobilereader/mobileobdreader/obddata/DataControl.java
@@ -0,0 +1,111 @@
+package org.souza.obdmobilereader.obdmobilereader.mobileobdreader.obddata;
+
+import android.os.Handler;
+import java.util.ArrayList;
+
+
+public class DataControl {
+
+    //Instance Vales
+
+    private ArrayList<DataGen> gauges;
+    private valueGenerator valGen;
+    private final Handler gHanlder;
+
+    public DataControl() {
+        this.gHanlder = null;
+        this.gauges   = null;
+        this.valGen   = null;
+    }
+
+    public DataControl(ArrayList<DataGen> gauges, Handler gHanlder) {
+        this.gauges   = gauges;
+        this.gHanlder = gHanlder;
+        this.valGen   = null;
+    }
+
+    public ArrayList<DataGen> getGauges() {
+        return gauges;
+    }
+
+    public void setGauges(ArrayList<DataGen> gauges) {
+        this.gauges = gauges;
+    }
+
+    public DataGen getSpecificGauge(int id) {
+        return gauges.get(id);
+    }
+
+    public void setGaugeState(int id, boolean state) {
+        gauges.get(id).setState(state);
+    }
+
+    public void startGenerator(){
+        this.valGen =  new valueGenerator(false);
+        this.valGen.start();
+    }
+
+    private class valueGenerator extends Thread {
+        private boolean isRunning;
+        private Object mPauseLock;
+
+        public valueGenerator(boolean state){
+            this.isRunning = state;
+            this.mPauseLock = new Object();
+        }
+
+        public boolean getIsRunning(){return this.isRunning;}
+
+        @Override
+        public void run(){
+            boolean valInc = true;
+            DataGen tmp;
+            float val = 0.f;
+            this.onResume();
+            int x = 15;
+            int size = gauges.size();
+            try {
+                while(this.isRunning){
+                    val = (float) Math.cos(((float)x/10));
+                    for(int i =0;i< size-5;i++) {
+                        tmp = gauges.get(i);
+                        int valInt = (int) (val*tmp.getMaxVal());
+                        gHanlder.obtainMessage(0, tmp.getGaugeID(), valInt).sendToTarget();
+                    }
+                    if (valInc) {
+                        x -= 1;
+                    } else{
+                        x += 1;
+                    }
+                    if(  x == 0 && valInc){
+                        valInc = false;
+                    }else if( x == 15 && !valInc){
+                        valInc = true;
+                    }
+                    Thread.sleep(1500);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void onPause() {
+            synchronized (mPauseLock) {
+                isRunning = false;
+            }
+
+        }
+
+        public void onResume() {
+            synchronized (mPauseLock) {
+                isRunning = true;
+                mPauseLock.notifyAll();
+            }
+        }
+
+
+    }
+
+
+}
+
