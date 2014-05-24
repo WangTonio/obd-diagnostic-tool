829781ebfe0df5f0be3caad3b360f0fc97718c59
ajsouza88 <ajsouza88@9e2c58d2-b951-7405-26da-efba02381a61>
2014-05-24 01:25:19 +0000

    Commit Fixed some bugs in application life cycle

--- branches/Souza52114/src/main/java/org/souza/obdmobilereader/obdmobilereader/mobileobdreader/OBD_Main.java
+++ branches/Souza52114/src/main/java/org/souza/obdmobilereader/obdmobilereader/mobileobdreader/OBD_Main.java
@@ -264,9 +264,6 @@ public class OBD_Main extends FragmentActivity implements DtcLookUpFragment.OnFr
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             Intent intent;
